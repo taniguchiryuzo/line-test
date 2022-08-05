743a6e47d3b09bb479f40d277621037d6989b890
taniguchiryuzo <kousei0038@gmail.com>
2022-08-05 22:06:17 +0900

    Procfile for Heroku

--- app/Services/LineBotService.php
+++ app/Services/LineBotService.php
@@ -22,6 +22,8 @@ class LineBotService
     {
         $this->httpClient = new CurlHTTPClient('<channel access token>');
         $this->bot = new LINEBot($this->httpClient, ['channelSecret' => '<channel secret>']);
+        $this->httpClient = new CurlHTTPClient(config('app.line_channel_access_token'));
+        $this->bot = new LINEBot($this->httpClient, ['channelSecret' => config('app.line_channel_secret')]);
     }
 
     /**
@@ -34,6 +36,62 @@ class LineBotService
      */
     public function eventHandler(Request $request): int
     {
-        return 200;
+        // 署名を検証しLINE以外からのリクエストを受け付けない。
+        $this->validateSignature($request);
+
+        // リクエストをEventオブジェクトに変換する
+        $events = $this->bot->parseEventRequest($request->getContent(), $request->header('x-line-signature'));
+
+        foreach ($events as $event) {
+            // Reply token無しでは返信できないため定義しておく
+            $reply_token = $event->getReplyToken();
+            // 無効な操作があったときに送るメッセージ
+            $message_builder = new TextMessageBuilder('Invalid operation. 無効な操作です。');
+            // アクションした人のLINEのユーザーID
+            $line_user_id = $event->getUserId();
+
+            switch (true) {
+                    // テキストメッセージを受信した場合
+                case $event instanceof TextMessage:
+                    break;
+                    // 選択肢を選んだ場合
+                case $event instanceof PostbackEvent:
+                    break;
+            }
+        }
+
+        // LINEに返信
+        $response = $this->bot->replyMessage($reply_token, $message_builder);
+
+        // 送信に失敗したらログに吐いておく
+        if (!$response->isSucceeded()) {
+            \Log::error('Failed!' . $response->getHTTPStatus() . ' ' . $response->getRawBody());
+        }
+
+        return $response->getHTTPStatus();
     }
 }
+//     /**
+//      * LINEの署名確認
+//      *
+//      * @param Request
+//      * @return void
+//      * @throws HttpException 
+//      */
+//     public function validateSignature(Request $request): void
+//     {
+//         // リクエストヘッダーについてくる実際の署名
+//         $signature = $request->header('x-line-signature');
+//         if ($signature === null) {
+//             abort(400);
+//         }
+
+//         // LINEチャネルシークレットとリクエストボディを基に署名を生成
+//         $hash = hash_hmac('sha256', $request->getContent(), config('app.line_channel_secret'), true);
+//         $expect_signature = base64_encode($hash);
+
+//         // 実際の署名と生成した署名が同じであれば検証OK
+//         if (!hash_equals($expect_signature, $signature)) {
+//             abort(400);
+//         }
+//     }
